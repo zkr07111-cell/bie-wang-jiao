1e21336bca534797c1e24dac7e67982ea1b3e770
zkr07111-cell <zkr07111@gmail.com>
2026-09-17 16:47:30 +0800

    Fix PWA cache refresh behavior

--- sw.js
+++ sw.js
@@ -1,9 +1,40 @@
-const CACHE='homework-pwa-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
-    const copy=resp.clone(); caches.open(CACHE).then(c=>c.put(e.request,copy)); return resp;
-  }).catch(()=>caches.match('./index.html'))));
+const CACHE='homework-pwa-v4';
+const CORE=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+      .then(()=>self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  const req=event.request;
+  if(req.mode==='navigate'){
+    event.respondWith(
+      fetch(req)
+        .then(resp=>{
+          const copy=resp.clone();
+          caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
+          return resp;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    fetch(req)
+      .then(resp=>{
+        const copy=resp.clone();
+        caches.open(CACHE).then(cache=>cache.put(req,copy));
+        return resp;
+      })
+      .catch(()=>caches.match(req))
+  );
 });
