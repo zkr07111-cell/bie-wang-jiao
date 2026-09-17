32665c7fdf4e34e16af4e4af7f9d86b0a9205e08
zkr07111-cell <zkr07111@gmail.com>
2026-09-17 16:55:19 +0800

    Refresh PWA after Workers AI migration

--- sw.js
+++ sw.js
@@ -1,40 +1,5 @@
-const CACHE='homework-pwa-v4';
+const CACHE='homework-pwa-v5';
 const CORE=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-
-self.addEventListener('install',event=>{
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));
-});
-
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
-      .then(()=>self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch',event=>{
-  const req=event.request;
-  if(req.mode==='navigate'){
-    event.respondWith(
-      fetch(req)
-        .then(resp=>{
-          const copy=resp.clone();
-          caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
-          return resp;
-        })
-        .catch(()=>caches.match('./index.html'))
-    );
-    return;
-  }
-  event.respondWith(
-    fetch(req)
-      .then(resp=>{
-        const copy=resp.clone();
-        caches.open(CACHE).then(cache=>cache.put(req,copy));
-        return resp;
-      })
-      .catch(()=>caches.match(req))
-  );
-});
+self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{const req=event.request;if(req.mode==='navigate'){event.respondWith(fetch(req).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return resp}).catch(()=>caches.match('./index.html')));return}event.respondWith(fetch(req).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(cache=>cache.put(req,copy));return resp}).catch(()=>caches.match(req)))});
