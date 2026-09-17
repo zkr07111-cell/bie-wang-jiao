f65df927b8fdaa0c5ccdf3692bb1d50a4008cea7
zkr07111-cell <zkr07111@gmail.com>
2026-09-17 16:42:31 +0800

    Refresh cache for OCR release

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='homework-pwa-v2';
+const CACHE='homework-pwa-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
