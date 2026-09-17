a1e2b9afeac82caf0face809ff67457076ed8d1a
zkr07111-cell <zkr07111@gmail.com>
2026-09-17 17:42:59 +0800

    Bump PWA cache for timing diagnostics

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='homework-pwa-v7';
+const CACHE='homework-pwa-v8';
 const CORE=['./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>{self.skipWaiting();event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
